c790a2bd5bfe68ae4f0740108ce40f279ac15a85
instellate <love.mann@elev.ga.ntig.se>
2024-12-02 13:51:14 +0100

    Implementing adding or editing This implements the function ``AddOrEdit`` which lets you add or edit to an List

--- TaskSplitter/Program.cs
+++ TaskSplitter/Program.cs
@@ -109,8 +109,89 @@ public class Program
         throw new NotImplementedException();
     }
 
-    private static void AddOrEdit(List<string> dataPeople, string person)
+    private static void AddOrEdit(List<string> data, string typeName)
     {
-        throw new NotImplementedException();
+        // Data är för data, typeName är för att printa vilken typ av sak som används
+        // Gör så har för att hindra dupliserad kod.
+        
+        // Ge en beskrivning över hur det fungerar att spara data
+        Console.Clear();
+        Console.WriteLine(
+            "Välj ett nummer för att redigera dem (eller ge inget på nya namnet om du vill ta bort).");
+        while (true)
+        {
+            // Printa ut vad olika saker funkar. Skriv `a` för att  lägga till en {typeName}, q för tillbaka.
+            // Siffror för att redigera en användare 
+            Console.WriteLine($"Skriv a om du vill lägga till en {typeName.ToLower()}.");
+            Console.WriteLine("Skriv q om du vill gå tillbaka.");
+            for (int i = 0; i < data.Count; i++)
+            {
+                // Olika personer som finns och deras position
+                Console.WriteLine($"{i + 1}. {data[i]}");
+            }
+
+            Console.Write("Input: ");
+
+            // Läs input från användare
+            string? input = Console.ReadLine();
+            switch (input)
+            {
+                // Om det är q, lämna loopen.
+                case "q":
+                    Console.Clear();
+                    return;
+                case "a":
+                    // Om det är a, fråga efter namn
+                    Console.Write("Namnet: ");
+                    string? name = Console.ReadLine();
+                    if (!string.IsNullOrWhiteSpace(name))
+                    {
+                        // Om namn är inte null, tom eller bara spaces. Trimma och lägg till det
+                        data.Add(name.Trim());
+                        Console.Clear();
+                        Console.WriteLine($"La till {name.Trim()}");
+                    }
+
+                    break;
+                default:
+                {
+                    // Om det är inget av ovanför. Försök se om det är ett nummer
+                    if (int.TryParse(input, out int index))
+                    {
+                        // Ta bort `1` från index eftersom positioner börjar på 0 istället för 1
+                        index--;
+                        // Om det är mindre än 0 (positoner mindre än 0 finns inte) eller om det är större än vad vi har, lämna
+                        if (index < 0 || index >= data.Count)
+                        {
+                            Console.Clear();
+                            Console.WriteLine($"{typeName} finns inte");
+                        }
+                        else
+                        {
+                            // Kopiera ut det gamla namnet och fråga om nya namnet
+                            string oldName = data[index];
+                            Console.Write("Nytt namn: ");
+                            string? newName = Console.ReadLine();
+                            if (!string.IsNullOrWhiteSpace(newName))
+                            {
+                                // Om stringen är inte tom, döpp om {typeName} till ett nytt namn
+                                data[index] = newName;
+                                Console.Clear();
+                                Console.WriteLine($"{oldName} heter nu {newName.Trim()}");
+                            }
+                            else
+                            {
+                                // Om stringen är tom, ta bort {typeName}
+                                data.RemoveAt(index);
+                                Console.Clear();
+                                Console.WriteLine($"Tog bort {oldName}");
+                            }
+                        }
+                    }
+
+                    break;
+                }
+            }
+        }
     }
 }
